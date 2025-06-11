cf08d5334905aa0b76ca19e5b513ba8f4d90f474
wahfung <shi_yefeng@163.com>
2025-06-11 15:34:17 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import App from '@pages/App';
+import './style.css';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+if (!container) {
+  throw new Error('Failed to find the root element');
+}
+const root = createRoot(container);
+
+root.render(<App />);
---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-[#09F]">🐻 App component rendered</h1>
+    </>
+  );
+};
+
+export default App;
